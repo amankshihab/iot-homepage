42cb54641fe6801f5d541bb38fd2955c452f58bc
Aman K. Shihab <amanshihab276@gmail.com>
2022-10-20 19:52:30 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+import DayNightToggle from "react-day-and-night-toggle";
+
+function send_request(on_off) {
+  const Http = new XMLHttpRequest();
+  let url = "http://192.168.101.143:80";
+
+  if (on_off) {
+    url = url + "/ledon"
+  } else {
+    url = url + "/ledoff"
+  }
+
+  console.log(url)
+
+  Http.open("GET", url);
+  Http.send();
+
+  // Http.onreadystatechange = (e) => {
+  //   console.log(Http.responseText);
+  // };
+}
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <DayNightToggle
+          onChange={() => {
+            setIsDarkMode(!isDarkMode);
+            send_request(!isDarkMode);
+          }}
+          checked={isDarkMode}
+        />
       </header>
     </div>
   );
